579421958d39ff2b863cbfa0641652265f99ce1d
musty <musty2025x@gmail.com>
2026-04-23 02:58:28 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'GitOps Demo App',
+    version: process.env.APP_VERSION || '1.0.0'
+  });
+});
+
+app.listen(3000, () => console.log('Running on port 3000'));
